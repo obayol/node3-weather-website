bef2e103f8ec19e60b46443220c0eb4b589d0f62
Oğulcan Bayol <obayol@192.168.1.51>
2021-02-05 22:11:32 +0300

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,31 @@
+console.log('Client side javascript file is loaded')
+
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+    response.json().then((data) => {
+        console.log(data)
+    })
+})
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input') 
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+messageOne.textContent = 'From JavaScript'
+messageTwo.textContent = ''
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    messageOne.textContent = 'Loading...'
+    
+    fetch(`http://localhost:3000/weather?address=${search.value}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                messageOne.textContent = data.message
+                console.log(data.message)
+            }
+        })
+    })
+})
---
+++ src/app.js
@@ -0,0 +1,74 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+// const geocode = require('./utils/geocode')
+// const forecast = require('./utils/forecast')
+
+const app = express()
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+ 
+// Set up handlebars 
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Set up static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Oğulcan Bayol'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Oğulcan Bayol'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is helpful text',
+        title: 'Help',
+        name: 'Oğulcan Bayol',
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('error', {
+        title: 'Help 404',
+        errorMessage: 'Help article not found',
+        name: 'Oğulcan Bayol'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+             error: 'Address must be provided'
+        })
+    }
+    
+    res.send({
+        message: 'This is the generic message'
+    })
+})  
+
+app.get('*', (req, res) => {
+    res.render('error', {
+        title: '404',
+        errorMessage: 'Page not found',
+        name: 'Oğulcan Bayol'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
